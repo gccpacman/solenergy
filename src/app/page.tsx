4011e7c9e2b471a15cfb943f2fd4f5fe00622d91
Gccpacman <gccpacman@gmail.com>
2024-07-14 16:45:03 +0800

    replace index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 
 import {Metadata} from 'next';
-import Button from '@mui/material/Button';
-
+// import Button from '@mui/material/Button';
+import Blog from '@/components/blog/Blog'
+import Container from '@mui/material/Container'
 
 export const metadata: Metadata = {
     title:'Next.js'
 };
 
 export default function Page() {
-    return <Button variant="contained">Hello world</Button>;
+    // return <Button variant="contained">Hello world</Button>;
+    return (
+        <Container maxWidth="lg">
+            <Blog />
+        </Container>
+    )
 }
